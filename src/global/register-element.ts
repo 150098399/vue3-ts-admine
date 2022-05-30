9f33fc77d05f3b2b4fcf6cd8c1bea8203d1c9fa0
1500980399 <1500980399@qq.com>
2022-05-30 21:10:50 +0800

    feat(all): 集成element-plus

--- src/global/register-element.ts
+++ src/global/register-element.ts
@@ -1,7 +1,8 @@
 import { App } from 'vue'
 import 'element-plus/theme-chalk/base.css'
+import 'element-plus/dist/index.css'
 
-import { ElButton } from 'element-plus'
+import { ElButton } from 'element-plus/lib'
 
 const components = [ElButton]
 export default function (app: App): void {
