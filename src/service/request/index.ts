7009967cfc8eddb2db18192c89a0cad4057e5fa4
1500980399 <1500980399@qq.com>
2022-06-05 13:47:37 +0800

    feat(service): 网络请求拦截器封装

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,4 +1,29 @@
 import axios from 'axios'
-class HYRequest {}
+import type { AxiosInstance, AxiosRequestConfig } from 'axios'
+import type { HYRequestInterceptors, HYRequestConfig } from './type'
+
+class HYRequest {
+  instance: AxiosInstance
+  interceptors?: HYRequestInterceptors
+  constructor(config: HYRequestConfig) {
+    this.instance = axios.create(config)
+    this.interceptors = config.interceptors
+
+    this.instance.interceptors.request.use(
+      this.interceptors?.requestInterceptor,
+      this.interceptors?.requestInterceptorCatch
+    )
+    this.instance.interceptors.response.use(
+      this.interceptors?.responseInterceptor,
+      this.interceptors?.responseInterceptorCatch
+    )
+  }
+
+  request(config: AxiosRequestConfig): void {
+    this.instance.request(config).then((res) => {
+      console.log(res)
+    })
+  }
+}
 
 export default HYRequest
