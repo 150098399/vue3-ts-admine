6f94c78e36fc1758c61907f3af57f81e29a79c3d
lilongjie <18373146601@163.com>
2022-06-12 22:13:23 +0800

    feat(login): 登录页面功能开发

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
-    <el-button>gg</el-button>
+  <div class="login">
+    <login-panel></login-panel>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+import LoginPanel from './componets/login-panel.vue'
 export default defineComponent({
+  components: {
+    LoginPanel
+  },
   setup() {
     return {}
   }
 })
 </script>
-<style scoped></style>
->
+<style lang="less" scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: url('../../assets/img/login-bg.svg');
+}
+</style>
