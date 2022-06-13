ce644874e92dc7e150b16d4dbed66307a7c32bb7
lilongjie <18373146601@163.com>
2022-06-13 23:13:48 +0800

    feat(login): 登录校验

--- src/views/login/componets/login-panel.vue
+++ src/views/login/componets/login-panel.vue
@@ -6,40 +6,73 @@
         <el-tab-pane>
           <template #label>
             <span class="custom-tabs-label">
-              <el-icon><User /></el-icon>
-              <span>账号登录</span>
+              <el-icon style="vertical-align: middle" :size="15"
+                ><User
+              /></el-icon>
+              <span style="vertical-align: middle">账号登录</span>
             </span>
           </template>
-          <login-account></login-account>
+          <login-account ref="accountRef"></login-account>
         </el-tab-pane>
         <el-tab-pane>
           <template #label>
             <span class="custom-tabs-label">
-              <el-icon :size="15"><iphone /></el-icon>
-              <span>手机登录</span>
+              <el-icon style="vertical-align: middle" :size="15"
+                ><iphone
+              /></el-icon>
+              <span style="vertical-align: middle">手机登录</span>
             </span>
           </template>
-          <h2>手机登录</h2>
+          <login-phone></login-phone>
         </el-tab-pane>
       </el-tabs>
+
+      <div class="account-control">
+        <el-checkbox v-model="isKeepPassword">记住密码</el-checkbox>
+        <el-link type="primary">忘记密码</el-link>
+      </div>
+
+      <el-button type="primary" class="login-btn" @click="handleLoginClick"
+        >立即登录</el-button
+      >
     </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import LoginAccount from './login-account.vue'
+import LoginPhone from './login-phone.vue'
 export default defineComponent({
-  components: { LoginAccount },
+  components: { LoginAccount, LoginPhone },
   setup() {
-    return {}
+    const isKeepPassword = ref(true)
+    const accountRef = ref<InstanceType<typeof LoginAccount>>()
+
+    const handleLoginClick = () => {
+      console.log(1111)
+      accountRef.value?.loginAction()
+    }
+    return {
+      isKeepPassword,
+      handleLoginClick,
+      accountRef
+    }
   }
 })
 </script>
 <style lang="less" scoped>
 .login-panel {
-  width: 320px;
+  width: 350px;
   .title {
     text-align: center;
+  }
+  .account-control {
+    display: flex;
+    justify-content: space-between;
+  }
+  .login-btn {
+    width: 100%;
+    margin-top: 10px;
   }
 }
 </style>
