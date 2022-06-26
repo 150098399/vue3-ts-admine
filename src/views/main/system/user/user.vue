d1d31d34081cc33614c7d46e3f27e7e9de32c687
lilongjie <18373146601@163.com>
2022-06-26 20:34:27 +0800

    feat(user): form组件封装

--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,55 +1,20 @@
 <template>
   <div class="user">
     <div class="search">
-      <hy-form :formItems="formItems"></hy-form>
+      <hy-form v-bind="searchFormConfig" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HyForm, { IFormItem } from '@/base-ui/form'
-
+import HyForm from '@/base-ui/form'
+import { searchFormConfig } from './config/search.config'
 export default defineComponent({
   components: { HyForm },
   name: 'user',
   setup() {
-    const formItems: IFormItem[] = [
-      {
-        type: 'input',
-        label: 'id',
-        placeholder: '请输入id'
-      },
-      {
-        type: 'input',
-        label: '用户名 ',
-        placeholder: '请输入用户名'
-      },
-      {
-        type: 'password',
-        label: '密码',
-        placeholder: '请输入密码'
-      },
-      {
-        type: 'select',
-        label: '喜欢的运动',
-        placeholder: '请选择喜欢的运动',
-        options: [
-          { title: '篮球', value: 'basketball' },
-          { title: '足球', value: 'football' }
-        ]
-      },
-      {
-        type: 'datepicker',
-        label: '创建时间',
-        otherOptions: {
-          startPlaceholder: '开始时间',
-          endPlaceholder: '结束时间',
-          type: 'daterange'
-        }
-      }
-    ]
-    return { formItems }
+    return { searchFormConfig }
   }
 })
 </script>
